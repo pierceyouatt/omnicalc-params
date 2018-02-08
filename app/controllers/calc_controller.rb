458592c0423b9d0aec48181b47b6e5c567d6d20d
pierceyouatt <pierce.youatt@chicagobooth.edu>
2018-02-08 18:56:56 -0500

    Thursday night draft

---
+++ app/controllers/calc_controller.rb
@@ -0,0 +1,60 @@
+class CalcController < ApplicationController
+  def flex_square
+    @my_num = params.fetch("num").to_i
+    @square = @my_num**2
+    render("/omni_templates/flex_square_view.html.erb")
+  end
+  def flex_square_root
+    @my_num = params.fetch("num").to_i
+    @square_root = @my_num**0.5
+    render("/omni_templates/flex_square_root_view.html.erb")
+  end
+  def flex_payment
+    @my_apr = params.fetch("apr").to_f/100
+    @my_years = params.fetch("years").to_i
+    @my_principal = params.fetch("principal").to_i
+    @my_payment = (@my_apr/1200*@my_principal)/(1-((1+@my_apr/1200)**(-12*@my_years)))
+    render("/omni_templates/flex_payment_view.html.erb")
+  end
+  def new_square_form
+    render ("/omni_templates/new_square_form_view.html.erb")
+  end
+  def new_root_form
+    render ("/omni_templates/new_root_form_view.html.erb")
+  end
+  def new_payment_form
+    render ("/omni_templates/new_payment_form_view.html.erb")
+  end
+  def new_random_form
+    render ("/omni_templates/new_random_form_view.html.erb")
+  end
+  def square_results
+    @the_num = params.fetch("user_num").to_f
+    @the_square = @the_num**2
+    render("/omni_templates/square_results_view.html.erb")
+  end
+  def root_results
+    @the_num = params.fetch("user_num").to_f
+    @the_root = @the_num**0.5
+    render("/omni_templates/root_results_view.html.erb")
+  end
+  def payment_results
+    @my_apr = params.fetch("user_apr").to_f/100
+    @my_years = params.fetch("user_years").to_i
+    @my_principal = params.fetch("user_principal").to_i
+    @my_payment = (@my_apr/1200*@my_principal)/(1-((1+@my_apr/1200)**(-12*@my_years)))
+    render("/omni_templates/payment_results_view.html.erb")
+  end
+  def random_results
+    @low_num = params.fetch("user_min").to_i
+    @high_num = params.fetch("user_max").to_i
+    @result = rand(@high_num-@low_num)+@low_num
+    render("/omni_templates/random_results_view.html.erb")
+  end
+  def random_num
+    @low_num = params.fetch("low_num").to_i
+    @high_num = params.fetch("high_num").to_i
+    @result = rand(@high_num-@low_now)+@low_num
+    render("/omni_templates/flex_random_number_view.html.erb")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  get("/flexible/square/:num",{:controller => "calc", :action => "flex_square"})
+  get("/flexible/square_root/:num",{:controller => "calc", :action => "flex_square_root"})
+  get("/flexible/payment/:apr/:years/:principal",{:controller => "calc", :action => "flex_payment"})
+  get("flexible/random/:low_num/:high_num",{:controller => "calc", :action => "random_num"})
+  get("/square/new",{:controller => "calc", :action => "new_square_form"})
+  get("/root/new",{:controller => "calc", :action => "new_root_form"})
+  get("/payment/new",{:controller => "calc", :action => "new_payment_form"})
+  get("/random/new",{:controller => "calc", :action => "new_random_form"})
+  get("/square/results",{:controller => "calc", :action => "square_results"})
+  get("/root/results",{:controller => "calc", :action => "root_results"})
+  get("/payment/results",{:controller => "calc", :action => "payment_results"})
+  get("/random/results",{:controller => "calc", :action => "random_results"})
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
